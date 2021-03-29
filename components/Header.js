7a850c00d37fbb2a19c11ae030bbee07a560cb7b
Julienchilhagopian <julienchilhagopian@gmail.com>
2021-03-29 13:30:31 +0200

    Header - client side migration

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <section class="header">
+      <div>
+        <h1 class="radio-name">radio paradis</h1>
+        <p>Née au coeur du 10ème arrondissement, tournée vers le monde.</p>
+      </div>
+      <div>
+        <button class="song-request-btn">Proposer une musique !</button>
+      </div>
+    </section>
+  )
+}
