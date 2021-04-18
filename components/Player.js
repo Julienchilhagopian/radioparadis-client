0094e4d79b77a8f5ca4f094511fc8eac71d587ed
Julienchilhagopian <julienchilhagopian@gmail.com>
2021-04-18 18:04:17 +0200

    player animation

--- components/Player.js
+++ components/Player.js
@@ -1,9 +1,39 @@
 import styles from '../styles/Player.module.scss'
+import React, { Component } from 'react';
 
-export default function Player() {
-  return (
-    <section className={styles.player}>
-      <p>Radio player</p>
-    </section>
-  )
+class Player extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      isPlaying: false
+    };
+
+    this.togglePlay = this.togglePlay.bind(this);
+  }
+
+
+  togglePlay() {
+    this.setState(prevState => ({
+      isPlaying: !prevState.isPlaying
+    }));
+  }
+  
+  render() {
+    return (
+      <section className={styles.player}>
+        <div>
+          <button onClick={this.togglePlay} className={this.state.isPlaying ? (styles.playBtn + " " + styles.paused) : styles.playBtn }></button>
+        </div>
+        <div className={styles.currentInfos}>
+          <h1>PORTICO QUARTET - Knee deep in the north see</h1>
+          <h2>Knee deep in the north see</h2>
+        </div>
+      </section>
+    )
+  }
 } 
+
+
+export default Player
