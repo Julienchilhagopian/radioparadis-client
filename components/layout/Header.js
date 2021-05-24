f2e67e0b0ff384b3b772c0c91c49fa79a48c5211
Julienchilhagopian <julienchilhagopian@gmail.com>
2021-05-24 10:17:42 +0200

    bolder header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -19,10 +19,10 @@ class Header extends Component {
         <div className={styles.singleMood}>
           {currentMood}
         </div>
-        <div className={styles.multipleMood}>
-          <h3>07h00 - 10h00 : Musique de douche.</h3>
-          <h3>10h00 - 19h00 : Musique du monde.</h3>
-          <h3>19h00 - 00h00 : Musique de bouge.</h3>
+        <div className={styles.multipleMood}>         
+          <h3 className={this.props.isMorning ? (styles.bolder) : ''}>07h00 - 10h00 : Musique de douche.</h3>
+          <h3 className={this.props.isDay ? (styles.bolder) : '' }>10h00 - 19h00 : Musique du monde.</h3>
+          <h3 className={this.props.isNight ? (styles.bolder) : ''}>19h00 - 00h00 : Musique de bouge.</h3>
         </div>
 
         {/* <div>
