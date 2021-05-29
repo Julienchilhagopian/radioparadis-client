88f709a48881bbd349bd36368c9b8d18ffbd23e5
Julienchilhagopian <julienchilhagopian@gmail.com>
2021-05-29 09:27:05 +0200

    donate btn

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -25,6 +25,13 @@ class Footer extends Component {
         </div>
         <div className={styles.logo}>
           <h1>Radio Paradis</h1>
+          <div>
+            <form action="https://www.paypal.com/donate" method="post" target="_top">
+              <input type="hidden" name="hosted_button_id" value="2N3QVAFTRSAZL" />
+              <input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+              <img alt="" border="0" src="https://www.paypal.com/fr_FR/i/scr/pixel.gif" width="1" height="1" />
+            </form>
+          </div>
           <h3>Rue de paradis, 10ème arrondissement.</h3>
         </div>
       </section>
