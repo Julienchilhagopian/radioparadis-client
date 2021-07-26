b0172135d93ad82ee478584a4c8151e30e83ca1b
Julienchilhagopian <julienchilhagopian@gmail.com>
2021-07-27 01:33:20 +0200

    new positionning

--- components/layout/BehindRadio.js
+++ components/layout/BehindRadio.js
@@ -12,20 +12,55 @@ class BehindRadio extends Component {
   render() {
     return (
       <section>
-          <ColorHeader principalColor={this.props.principalColor} title='Derrière la radio' />
+          <div className={styles.headerContainer}>
+            <ColorHeader principalColor={this.props.principalColor} title='Derrière la radio'/>
+          </div> 
           <div className={styles.imagesContainer}>
             <div className={styles.imageCard}>
               <div className={styles.imageContent}>
-              <Image 
-                quality={40}
-                src="/../../public/DSC03104.JPG" 
-                alt="Logo" 
-                width={600} 
-                height={400} 
-              />
+                <Image 
+                  quality={40}
+                  src="https://raw.githubusercontent.com/Julienchilhagopian/radioparadis-client/main/public/logo-day.png" 
+                  alt="Logo" 
+                  width={600} 
+                  height={400} 
+                />
               </div>
               <div className={styles.imageDescription}>
+                <h2>La radio existe aussi grâce au Jardin.</h2>
+                <p>Jardin Villemin</p>
+              </div>
+            </div> 
 
+            <div className={styles.imageCardMiddle}>
+              <div className={styles.imageContent}>
+                <Image 
+                  quality={40}
+                  src="https://raw.githubusercontent.com/Julienchilhagopian/radioparadis-client/main/public/logo-day.png" 
+                  alt="Logo" 
+                  width={600} 
+                  height={400} 
+                />
+              </div>
+              <div className={styles.imageDescription}>
+                <h2>Enregistrement des jingles.</h2>
+                <p>Rue de Paradis</p>
+              </div>
+            </div> 
+
+            <div className={styles.imageCardEnd}>
+              <div className={styles.imageContent}>
+                <Image 
+                  quality={40}
+                  src="https://raw.githubusercontent.com/Julienchilhagopian/radioparadis-client/main/public/logo-day.png" 
+                  alt="Logo" 
+                  width={600} 
+                  height={400} 
+                />
+              </div>
+              <div className={styles.imageDescription}>
+                <h2>Enregistrement des jingles.</h2>
+                <p>Rue de Paradis</p>
               </div>
             </div> 
           
