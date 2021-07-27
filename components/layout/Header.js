6dc1c0cb40a095eb61680b44e0f9d844245b7d4d
Julienchilhagopian <julienchilhagopian@gmail.com>
2021-07-28 00:32:48 +0200

    small header modif

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -29,7 +29,7 @@ class Header extends Component {
           <div className={styles.multipleMood}>         
             <h3 className={this.props.isMorning ? (styles.bolder) : ''}>07h00 - 10h00 : Musique de douche.</h3>
             <h3 className={this.props.isDay ? (styles.bolder) : '' }>10h00 - 19h00 : Musique du monde.</h3>
-            <h3 className={this.props.isNight ? (styles.bolder) : ''}>19h00 - 00h00 : Musique qui bouge.</h3>
+            <h3 className={this.props.isNight ? (styles.bolder) : ''}>19h00 - 00h00 : Musique de bouge.</h3>
           </div>
         }
       
