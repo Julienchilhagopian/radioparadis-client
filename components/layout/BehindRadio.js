190686806cc269cd123325e195735f0c410d90b5
Julienchilhagopian <julienchilhagopian@gmail.com>
2021-07-30 18:22:47 +0200

    image size

--- components/layout/BehindRadio.js
+++ components/layout/BehindRadio.js
@@ -19,14 +19,14 @@ class BehindRadio extends Component {
             <div className={styles.imageCard}>
               <div className={styles.imageContent}>
                 <Image 
-                  quality={20}
+                  quality={10}
                   src="https://raw.githubusercontent.com/Julienchilhagopian/radioparadis-client/main/public/jardin-villemin.png" 
                   alt="Logo" 
                   layout="fill"
                 />
               </div>
               <div className={styles.imageDescription}>
-                <h2>La radio c'est aussi les conversations du jardin.</h2>
+                <h2>La radio c'est aussi les conversations du jardin</h2>
                 <p>Jardin Villemin</p>
               </div>
             </div> 
@@ -34,7 +34,7 @@ class BehindRadio extends Component {
             <div className={styles.imageCardMiddle}>
               <div className={styles.imageContent}>
                 <Image 
-                  quality={20}
+                  quality={10}
                   src="https://raw.githubusercontent.com/Julienchilhagopian/radioparadis-client/main/public/record-jingles.png" 
                   alt="Logo" 
                   layout="fill"
@@ -49,7 +49,7 @@ class BehindRadio extends Component {
             <div className={styles.imageCardEnd}>
               <div className={styles.imageContent}>
                 <Image 
-                  quality={20}
+                  quality={10}
                   src="https://raw.githubusercontent.com/Julienchilhagopian/radioparadis-client/main/public/rue-paradis.png" 
                   alt="Logo" 
                   layout="fill"
