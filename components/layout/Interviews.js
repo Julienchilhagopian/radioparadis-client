b0b94c808b0904fe75f3fdb4ab2ea2f5685c7f7c
Julienchilhagopian <julienchilhagopian@gmail.com>
2022-04-02 18:51:14 +0200

    link cave curieuse show

--- components/layout/Interviews.js
+++ components/layout/Interviews.js
@@ -14,13 +14,13 @@ class Interviews extends Component {
         "title": "Curieuse cave - 11 Rue de la Fidélité",
         "shop_name": "RENCONTRE DE QUARTIER DIMANCHE 20H",
         "link_img": "https://raw.githubusercontent.com/Julienchilhagopian/radioparadis-client/main/public/curieuse-cave-itw-min2.jpg",
-        "link_show": ""
+        "link_show": "https://soundcloud.com/radio-paradis-13/rencontre-de-quartier-curieuse-cave"
       },
       {
         "title": "Curieuse cave - 11 Rue de la Fidélité",
         "shop_name": "RENCONTRE DE QUARTIER DIMANCHE 20H",
         "link_img": "https://raw.githubusercontent.com/Julienchilhagopian/radioparadis-client/main/public/curieuse-cave-itw-min.jpg",
-        "link_show": ""
+        "link_show": "https://soundcloud.com/radio-paradis-13/rencontre-de-quartier-curieuse-cave"
       },
       {
         "title": "Les couleurs du 10",
