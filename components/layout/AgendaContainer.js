3a385e71c53fab619035a0db50fdf929d20483b5
Julienchilhagopian <julienchilhagopian@gmail.com>
2023-11-01 15:11:15 +0100

    fix display agenda

--- components/layout/AgendaContainer.js
+++ components/layout/AgendaContainer.js
@@ -14,24 +14,24 @@ class AgendaContainer extends Component {
         <div className={styles.AgendaContainer}>
           <h1>AGENDA</h1>
           <div className={styles.agendaData}>
-          { (!events.length) ?
-          <h3>...chargement de l'agenda...</h3>
-          :
-          events.slice(0,5).map((day, index) => (
-            <div key={index} className={styles.eventsDate}>
-              <h3>{day.date}</h3>
-              {
-                day.events.map((event, index) => (
-                  <div className={styles.agendaEvent} key={index}>
-                    <p>{event.cleanTime}</p>
-                    <h2>{event.title}</h2>
-                    <p>{event.description}</p>
-                  </div>
-                ))
-              }
-            </div>
-          ))
-          }
+            { (!events.length) ?
+            <h3>...chargement de l'agenda...</h3>
+            :
+            events.slice(0,5).map((day, index) => (
+              <div key={index} className={styles.eventsDate}>
+                <h3>{day.date}</h3>
+                {
+                  day.events.map((event, index) => (
+                    <div className={styles.agendaEvent} key={index}>
+                      <p>{event.cleanTime}</p>
+                      <h2>{event.title}</h2>
+                      <p>{event.description}</p>
+                    </div>
+                  ))
+                }
+              </div>
+            ))
+            }
           </div>
         </div>
       </section>
