6223356a5bd7992582eaa95051f8a44278d5d05e
Julienchilhagopian <julienchilhagopian@gmail.com>
2023-11-03 20:40:57 +0100

    update modale java

--- components/elements/EventModal.js
+++ components/elements/EventModal.js
@@ -14,20 +14,20 @@ class EventModal extends Component {
     return (
       <Modal title="" open={this.props.show} onCancel={this.props.handleModalClose}>
         <div className={styles.EventModalContent}>
-          <h1 >Radio Paradis à la Rotonde le 26 octobre !</h1>
+          <h1 >Radio Paradis à la Java le 10 Novembre avec Family Approved Records !</h1>
           <div className={styles.EventLink}>
             <a href="https://fb.me/e/1meVwuN7P" target="_blank"> &rarr; Lien event &larr; </a>
           </div>
           <div className={styles.EventPoster}>
             <Image
               quality={80}
-              src={"https://raw.githubusercontent.com/Julienchilhagopian/radioparadis-client/agenda/public/affiche_rotonde_octobre.jpg"}
+              src={"https://raw.githubusercontent.com/Julienchilhagopian/radioparadis-client/agenda/public/affiche_java_novembre.jpeg"}
               alt="Logo"
               width={286}
               height={407}
             />
           </div>
-          <p className={styles.EventModalCredits}>Affiche de Diane Thouve</p>
+          <p className={styles.EventModalCredits}></p>
         </div>
       </Modal>
     );
