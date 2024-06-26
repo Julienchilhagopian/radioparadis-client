65ece2b539a23cb7f3fa5e11c4a3bd19aee3a0dd
Julienchilhagopian <julienchilhagopian@gmail.com>
2024-06-26 21:44:59 +0200

    updt badaboum

--- components/elements/EventModal.js
+++ components/elements/EventModal.js
@@ -14,17 +14,17 @@ class EventModal extends Component {
     return (
       <Modal title="" open={this.props.show} onCancel={this.props.handleModalClose}>
         <div className={styles.EventModalContent}>
-          <h1>Radio Paradis au Transistor pour la fête de la musique  !</h1>
+          <h1>Radio Paradis à a Capsule du Badaboum</h1>
           <div className={styles.EventLink}>
-            <a href="https://www.facebook.com/share/CDqZPkCyWt1nVw4X/" target="_blank"> &rarr; Lien event &larr; </a>
+            <a href="https://www.facebook.com/share/HHQrJytKP5gp3MhT/" target="_blank"> &rarr; Lien event &larr; </a>
           </div>
           <div className={styles.EventPoster}>
             <Image
               quality={80}
-              src={"https://raw.githubusercontent.com/Julienchilhagopian/radioparadis-client/main/public/fdlm_event_fb.png"}
+              src={"https://raw.githubusercontent.com/Julienchilhagopian/radioparadis-client/main/public/badaboum.jpg"}
               alt="Logo"
-              width={400}
-              height={200}
+              width={450}
+              height={250}
             />
           </div>
           <p className={styles.EventModalCredits}></p>
