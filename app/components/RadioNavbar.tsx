c30f65c25f1275cd2164f4ba3a628e24fbe0630a
Julienchilhagopian <julienchilhagopian@gmail.com>
2024-07-31 14:40:48 +0200

    navbar v1

---
+++ app/components/RadioNavbar.tsx
@@ -0,0 +1,70 @@
+'use client'
+
+import styles from '../styles/navbar.module.scss';
+import React from "react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button } from "@nextui-org/react";
+
+
+
+
+function RadioNavbar() {
+
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "Archives",
+    "Se retrouver",
+    "À propos"
+  ];
+
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent className="lg:hidden">
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+        />
+      </NavbarContent>
+      <NavbarContent className="hidden lg:flex gap-4">
+        <NavbarItem>
+          <Link href="#">
+            Archives
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="#" aria-current="page">
+            Se retrouver
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="#">
+            À propos
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        <p>logo</p>
+      </NavbarContent>
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  )
+}
+
+export default RadioNavbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ const {nextui} = require("@nextui-org/react");
 module.exports = {
   content: [
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+
   ],
   theme: {
     extend: {},
